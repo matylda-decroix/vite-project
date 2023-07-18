1ae0e1cc69b21465551f470e7f833dd4c1aca826
MATYLDA DECROIX <matylda.decroix@gmail.com>
2023-07-18 19:20:20 +0200

    Fetching exemplary data with React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
 import "./App.css";
+import { useQuery } from "react-query";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
 
 function App() {
+  const { data } = useQuery(
+    "users",
+    (): Promise<User> =>
+      fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+        res.json()
+      ),
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  );
+  console.log(data);
+
   return (
     <>
-      <div>Hello world</div>
-      <div>Hello world</div>
+      {data?.map((user: User) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
     </>
   );
 }
